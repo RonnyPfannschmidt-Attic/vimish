5402ec43c16357574b9e34f8208a26fd7c86dd39
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2009-05-16 16:58:59 +0200

    add buffer slicing

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -12,3 +12,19 @@ def test_buffer_line_count(engine):
     buffer.text = "one line"
     assert len(buffer) == 1
 
+
+def test_buffer_slice(engine):
+    buffer = engine.create_buffer()
+    buffer.text = "Test text\n\nline2"
+    assert len(buffer) == 3
+
+    assert buffer[1] == "\n"
+
+    print repr(buffer.text)
+
+    buffer[1] = "Test\n"
+
+    print repr(buffer.text)
+
+    assert buffer[1] == "Test\n"
+    assert buffer[2] == "line2"
