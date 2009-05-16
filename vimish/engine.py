6f2d755526988ad1e87dd5cff5b6661d138dfe96
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2009-05-16 16:58:59 +0200

    add buffer slicing

--- vimish/engine.py
+++ vimish/engine.py
@@ -32,6 +32,25 @@ class Buffer(object):
     def __len__(self):
         return self.text_buffer.get_line_count()
 
+    def __getitem__(self, item):
+        start = self.text_buffer.get_iter_at_line(item)
+        if item+1 < len(self):
+            end = self.text_buffer.get_iter_at_line(item +1)
+        else:
+            end = self.text_buffer.get_end_iter()
+        return self.text_buffer.get_slice(start, end)
+
+    def __setitem__(self, item, value):
+        start = self.text_buffer.get_iter_at_line(item)
+        end = self.text_buffer.get_iter_at_line(item +1)
+
+        self.text_buffer.select_range(start, end)
+        self.text_buffer.delete_selection(False, False)
+
+        start = self.text_buffer.get_iter_at_line(item)
+        self.text_buffer.insert(start, value)
+
+
 class Engine(object):
 
     def create_buffer(self):
