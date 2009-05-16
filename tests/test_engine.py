860f89f4c4abdd10817884f96797a7eba7f8c041
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2009-05-16 17:33:58 +0200

    more slicing tests + support

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -13,7 +13,7 @@ def test_buffer_line_count(engine):
     assert len(buffer) == 1
 
 
-def test_buffer_slice(engine):
+def test_buffer_setitem_and_getitem(engine):
     buffer = engine.create_buffer()
     buffer.text = "Test text\n\nline2"
     assert len(buffer) == 3
@@ -28,3 +28,16 @@ def test_buffer_slice(engine):
 
     assert buffer[1] == "Test\n"
     assert buffer[2] == "line2"
+
+
+def test_buffer_get_slice_and_set_slice(engine):
+    buffer = engine.create_buffer()
+    buffer.text = "Test\nslicing\nwell"
+    assert buffer[:2] == ["Test\n", "slicing\n"]
+    assert buffer[2:] == ['well']
+
+
+    buffer[:2] = ['more\n', 'slicing\n', 'helps\n']
+    assert buffer[2] == "helps\n"
+
+
