94318d9b97b829c165c78b825d778c4ead514175
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2009-05-16 16:30:44 +0200

    more line count tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -2,9 +2,13 @@
 
 def test_buffer_line_count(engine):
     buffer = engine.create_buffer()
+
     buffer.text = "Test\ntest2"
     assert len(buffer) == 2
+
     buffer.append("\n")
     assert len(buffer) == 3
 
+    buffer.text = "one line"
+    assert len(buffer) == 1
 
