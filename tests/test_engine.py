4405dca64e3e6edd3c338d05b406531921dcd1e0
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2009-05-16 16:28:30 +0200

    initial buffer stuff

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,4 +1,10 @@
 
 
-def test_create_buffer(engine):
+def test_buffer_line_count(engine):
     buffer = engine.create_buffer()
+    buffer.text = "Test\ntest2"
+    assert len(buffer) == 2
+    buffer.append("\n")
+    assert len(buffer) == 3
+
+
