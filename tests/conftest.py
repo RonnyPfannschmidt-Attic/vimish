b74b59dee6cae68aa275b9c162b1d7a5bc5c26e2
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2009-05-17 09:00:02 +0200

    refactor buffer tests, add buffer numbers

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,10 @@
 
-from vimish.engine import Engine
+from vimish.engine import Engine, Buffer
 
 
 class ConftestPlugin:
     def pytest_funcarg__engine(self, request):
         return Engine()
+
+    def pytest_funcarg__buffer(self, request):
+        return Buffer(Engine())
